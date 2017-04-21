79295df6cfbaf4c43c922607df9af2bcdb8bb4af
ak1t0 <aktoo3097@gmail.com>
2017-04-21 14:55:15 +0900

    fix parameter

--- cookbooks/dotfiles/default.rb
+++ cookbooks/dotfiles/default.rb
@@ -1,12 +1,12 @@
 git "clone dotfiles" do
   user "#{node[:user]}"
-  destination "/home/#{node[:user]}/Works/dotfiles"
+  destination "/home/#{node[:user]}/#{node[:dir]}/dotfiles"
   repository "https://github.com/#{node[:user]}/dotfiles.git"
-  not_if "test -d /home/#{node[:user]}/Works/dotfiles"
+  not_if "test -d /home/#{node[:user]}/#{node[:dir]}/dotfiles"
 end
 
 execute "dotfiles setup" do
   user "#{node[:user]}"
-  cwd "/home/#{node[:user]}/Works/dotfiles"
+  cwd "/home/#{node[:user]}/#{node[:dir]}/dotfiles"
   command "./write.sh"
 end
